3f2dfd080ef1ce3ca005c30013ef596fbded5bf7
Lucas Ferreira <lsferreirapel@gmail.com>
2021-11-22 21:11:15 -0300

    🔧 Update app module and schema graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,11 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { BrandsModule } from './modules/brands/brands.module';
+import { ColorsModule } from './modules/colors/colors.module';
+import { ProductVariantModule } from './modules/product-variant/product-variant.module';
 
 @Module({
   imports: [
@@ -14,6 +19,11 @@ import { UsersModule } from './modules/users/users.module';
     }),
     TypeOrmModule.forRoot(),
     UsersModule,
+    ProductsModule,
+    CategoriesModule,
+    BrandsModule,
+    ColorsModule,
+    ProductVariantModule,
   ],
   controllers: [AppController],
   providers: [AppService],
