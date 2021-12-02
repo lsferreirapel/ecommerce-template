f23d7bb57bd97fdf41fffbb5bffe2094f1c14639
Lucas Ferreira <lsferreirapel@gmail.com>
2021-12-01 21:41:12 -0300

    🌱 Add seed migrations

---
+++ migrations/1638405181411-SeedRoles.ts
@@ -0,0 +1,21 @@
+
+import { RolesEnum } from 'src/common/enums/roles.enum';
+import { Role } from 'src/modules/roles/entities/role.entity';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+const ROLES = Object.values(RolesEnum);
+
+export class SeedRoles1607709416292 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const RoleRepo = queryRunner.connection.getRepository(Role);
+    await RoleRepo.save(ROLES.map(role => ({ name: role })));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const RoleRepo = queryRunner.connection.getRepository(Role);
+    const roles = await RoleRepo.find({
+      where: ROLES.map(role => ({ name: role })),
+    });
+    RoleRepo.remove(roles);
+  }
+}
