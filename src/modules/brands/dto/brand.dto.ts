c63eb4533b12e198890b46493b1a61197c056117
Lucas Ferreira <lsferreirapel@gmail.com>
2021-12-01 21:18:15 -0300

    🎨 Update brand module

--- src/modules/brands/dto/brand.dto.ts
+++ src/modules/brands/dto/brand.dto.ts
@@ -1,8 +1,10 @@
-import { FilterableField } from '@nestjs-query/query-graphql';
+import { FilterableField, FilterableOffsetConnection } from '@nestjs-query/query-graphql';
 import { ObjectType } from '@nestjs/graphql';
 import { BaseDTO } from 'src/modules/bases/dto/base.dto';
+import { ProductDTO } from 'src/modules/products/dto/product.dto';
 
 @ObjectType('Brand')
+@FilterableOffsetConnection('products', () => ProductDTO, { nullable: true })
 export class BrandDTO extends BaseDTO {
   @FilterableField()
   name: string;
--- src/modules/brands/entities/brand.entity.ts
+++ src/modules/brands/entities/brand.entity.ts
@@ -1,8 +1,12 @@
 import { BaseEntity } from 'src/modules/bases/entities/base.entity';
-import { Column, Entity } from 'typeorm';
+import { Product } from 'src/modules/products/entities/product.entity';
+import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity()
 export class Brand extends BaseEntity {
   @Column()
   name: string;
+
+  @OneToMany(() => Product, (product) => product.brand, { nullable: true })
+  products: Product[]
 }
